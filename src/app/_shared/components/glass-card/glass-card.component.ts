44d5f39718cf291573740b3228b3e2fa7f8c8696
Guilherme <gui.bm.1995@gmail.com>
2022-11-27 04:02:47 -0300

    first commit

---
+++ src/app/_shared/components/glass-card/glass-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'glass-card',
+  templateUrl: './glass-card.component.html',
+  styleUrls: ['./glass-card.component.scss']
+})
+export class GlassCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { MaterialModule } from './_shared/modules/material.module';
+import { GlassCardComponent } from './_shared/components/glass-card/glass-card.component';
+import { WeatherComponent } from './_shared/components/weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GlassCardComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
