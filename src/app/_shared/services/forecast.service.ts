79ff24ed16e9b68ec61267da3828b368a601e0e0
Guilherme <gui.bm.1995@gmail.com>
2022-12-19 01:56:13 -0300

    reverted to the old method

--- src/app/_shared/services/forecast.service.ts
+++ src/app/_shared/services/forecast.service.ts
@@ -14,8 +14,7 @@ export class ForecastService {
     const hourly = 'temperature_2m,precipitation';
     const currentWeather = true;
     const today = new Date().toISOString().split('T')[0];
-    let envUrl = environment.production ? environment.url : '/guide/';
 
-    return this.httpClient.get<Forecast>(`${envUrl}?timezone=America%2FSao_Paulo&hourly=${hourly}&current_weather=${currentWeather}&start_date=${today}&end_date=${today}&latitude=${latitude}&longitude=${longitude}`)
+    return this.httpClient.get<Forecast>(`${environment.url}?timezone=America%2FSao_Paulo&hourly=${hourly}&current_weather=${currentWeather}&start_date=${today}&end_date=${today}&latitude=${latitude}&longitude=${longitude}`)
   }
 }
