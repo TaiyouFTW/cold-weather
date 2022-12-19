63ff068539ec81df2506fb64683464dc65a92044
Guilherme <gui.bm.1995@gmail.com>
2022-12-19 00:41:26 -0300

    api config

---
+++ api/weather.ts
@@ -0,0 +1,22 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import request from 'request';
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    if (req.method === 'GET') {
+        request(
+            'https://api.open-meteo.com/v1/forecast',
+            {
+                method: 'GET',
+            }, (error: any, response: any, body: any) => {
+                if (response.statusCode == 200) {
+                    res.status(200).send(response.body);
+                } else {
+                    res.status(response.statusCode).send(error.message);
+                }
+            }
+        );
+    }
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'https://api.open-meteo.com/v1/forecast'
+  url: '/api/'
 };
