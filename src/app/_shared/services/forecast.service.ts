61277a0aa74fcbd9fea931fcad25c8dc479a3b6f
Guilherme <gui.bm.1995@gmail.com>
2022-12-19 00:58:50 -0300

    fixed proxy env

--- src/app/_shared/services/forecast.service.ts
+++ src/app/_shared/services/forecast.service.ts
@@ -14,7 +14,7 @@ export class ForecastService {
     const hourly = 'temperature_2m,precipitation';
     const currentWeather = true;
     const today = new Date().toISOString().split('T')[0];
-    let envUrl = environment.production ? '/guide/' : environment.url;
+    let envUrl = environment.production ? environment.url : '/guide/';
 
     return this.httpClient.get<Forecast>(`${envUrl}?timezone=America%2FSao_Paulo&hourly=${hourly}&current_weather=${currentWeather}&start_date=${today}&end_date=${today}&latitude=${latitude}&longitude=${longitude}`)
   }
